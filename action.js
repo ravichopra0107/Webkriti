1a1a257290b44f59dc2adac05d0f5d02f276da2a
ravichopra0107 <56033256+ravichopra0107@users.noreply.github.com>
2020-03-07 18:26:44 +0530

    Update action.js

--- action.js
+++ action.js
@@ -3,21 +3,21 @@ var i=0,i1=0;
 var j=180,j1=180;
 for(var i=0;i<4;i++)
 {
-  z[i].style.position='relative';
+  z[i].style.position='absolute';
 }
 setInterval(full,30);
 function full()
 {
 
   var x,y,x1,y1;
-  x=80+220.5165*Math.cos(i*(Math.PI)/180);
-  y=-40+220.5165*Math.sin(i*(Math.PI)/180);
-  x1=-88+220.5165*Math.cos(j*(Math.PI)/180);
-  y1=83+220.5165*Math.sin(j*(Math.PI)/180);
-  z[0].style.top=y+'px';
-  z[0].style.left=x+'px';
-  z[1].style.top=y1+'px';
-  z[1].style.left=x1+'px';
+  x=6+13.5165*Math.cos(i*(Math.PI)/180);
+  y=12+23.5165*Math.sin(i*(Math.PI)/180);
+  x1=6+13.5165*Math.cos(j*(Math.PI)/180);
+  y1=12+23.5165*Math.sin(j*(Math.PI)/180);
+  z[0].style.top=y+'vh';
+  z[0].style.left=x+'vw';
+  z[1].style.top=y1+'vh';
+  z[1].style.left=x1+'vw';
     i++;
     j++;
 }
@@ -26,14 +26,14 @@ function full2()
 {
 
   var x,y,x1,y1;
-  x=-100+450.5165*Math.cos(i1*(Math.PI)/180);
-  y=200+380.5165*Math.sin(i1*(Math.PI)/180);
-  x1=-44+450.5165*Math.cos(j1*(Math.PI)/180);
-  y1=350+380.5165*Math.sin(j1*(Math.PI)/180);
-  z[2].style.top=y+'px';
-  z[2].style.left=x+'px';
-  z[3].style.top=y1+'px';
-  z[3].style.left=x1+'px';
+  x=10+32.5165*Math.cos(i1*(Math.PI)/180);
+  y=32+39.5165*Math.sin(i1*(Math.PI)/180);
+  x1=10+32.5165*Math.cos(j1*(Math.PI)/180);
+  y1=32+39.5165*Math.sin(j1*(Math.PI)/180);
+  z[2].style.top=y+'vh';
+  z[2].style.left=x+'vw';
+  z[3].style.top=y1+'vh';
+  z[3].style.left=x1+'vw';
     i1++;
     j1++;
 }
